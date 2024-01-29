b69f7e52bfcce06c11b75b176b8b42d92b368927
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-01-29 19:22:22 +0000

    variables

---
+++ Basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 1212121
+let accountName = "Harshit"
+var accountType = "Savings"
+accountCity = "Lucknow"
+
+accountName = "Harshoi"
+accountType = "Current"
+accountCity = "Bhopal"
+
+console.table([accountId, accountName, accountType, accountCity])
