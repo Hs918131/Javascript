4a789ede60ab870abf7758afba8ba62ecd735278
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-01-29 19:49:13 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as new version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3)
+
+console.log("Harshit")
+
+//Number : 2 to power of 54
+// BigINT : Higher Numbers
+//Strings : ""
+//Boolean : True or False
+//Null : Standalone Value
+//Undefined : 
+//Symbol : Uniqueness
+
+//Object
+//Array
+
