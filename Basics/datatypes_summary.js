f6150e8da6911ee294cc55556f5e90ed754bd445
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-01-30 17:57:40 +0000

    update

--- Basics/datatypes_summary.js
+++ Basics/datatypes_summary.js
@@ -35,3 +35,6 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//Stack(Primitive Data Type) & Heap(Non-Primitive Data Type)
+//Stack returns change only in the copy, But Heap points to the original Data. 
