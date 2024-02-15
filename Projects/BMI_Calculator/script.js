5c1dac9368cdee7ce784576ce0eddc0bb1f4cbef
Harshit Sharma <91822610+Hs918131@users.noreply.github.com>
2024-02-15 15:46:48 +0530

    Update script.js

--- Projects/BMI_Calculator/script.js
+++ Projects/BMI_Calculator/script.js
@@ -1,6 +1,4 @@
 const form = document.querySelector('form');
-// this usecase will give you empty
-// const height = parseInt(document.querySelector('#height').value)
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -15,8 +13,19 @@ form.addEventListener('submit', function (e) {
     results.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
-    results.innerHTML = `<span>${bmi}</span>`;
+    const uw = document.querySelector('#uw').innerHTML;
+    const nr = document.querySelector('#nr').innerHTML;
+    const ow = document.querySelector('#ow').innerHTML;
+
+    const remark = function (remark) {
+      if (bmi < 18.6) {
+        return (remark = 'UnderWeight');
+      } else if (bmi >= 18.6 && bmi <= 24.9) {
+        return (remark = 'Normal');
+      } else {
+        return (remark = 'Overweight');
+      }
+    };
+    results.innerHTML = `<span>${bmi} -: ${remark()}</span>`;
   }
 });
-
